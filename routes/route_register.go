ef5ab81c3050c9b1919d67cc44eef488e3a7349d
Sotirios Mantziaris <s.mantziaris@live.com>
2016-03-16 01:14:03 +0200

    created route register

--- routes/route_register.go
+++ routes/route_register.go
@@ -1,4 +1,4 @@
-package main
+package routes
 
 import (
 	"regexp"
@@ -21,37 +21,42 @@ type RouteRegistration struct {
 	Type       RequestType
 }
 
+// Register contains the route registrations
+var Register = RouteRegister{
+	routes: make([]RouteRegistration, 0),
+}
+
 // RouteRegister contains the registered routes
 type RouteRegister struct {
-	Routes []RouteRegistration
+	routes []RouteRegistration
 }
 
 // Register a route
 func (rr *RouteRegister) Register(method string, requestType RequestType, routeRegex string, response string) error {
-	
-    r, err := regexp.Compile(routeRegex)
+
+	r, err := regexp.Compile(routeRegex)
 	if err != nil {
 		return err
 	}
 
-	rr.Routes = append(rr.Routes, RouteRegistration{
+	rr.routes = append(rr.routes, RouteRegistration{
 		HTTPMethod: method,
 		Response:   response,
 		Type:       requestType,
 		RouteRegex: r,
-	})    
-    return nil
+	})
+	return nil
 }
 
 // Clear the register
 func (rr *RouteRegister) Clear(r RouteRegistration) {
-	rr.Routes = nil
+	rr.routes = nil
 }
 
 // Match a registration
 func (rr *RouteRegister) Match(method string, requestType RequestType, route string) (bool, string) {
 
-	for _, r := range rr.Routes {
+	for _, r := range rr.routes {
 
 		if r.HTTPMethod == method || r.Type == requestType || r.RouteRegex.MatchString(route) {
 			return true, r.Response
