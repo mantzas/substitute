c59b467d7c0870e09e80dd053c0db6f1dea3223c
Sotirios Mantziaris <s.mantziaris@live.com>
2016-03-20 20:22:21 +0200

    finalized test all packages script

--- testallpackages.sh
+++ testallpackages.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -ev
 echo "mode: count" > coverage.out
 
 for pkg in $(go list ./... | grep -v '/vendor/')
 do
     go test -v -covermode=count -coverprofile=coverage_tmp.out $pkg
     
-    if [ -f $FILE ];
+    if [ -f coverage_tmp.out ];
     then
         tail -n +2 coverage_tmp.out >> coverage.out
     fi
