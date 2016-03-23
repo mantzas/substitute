a32e39e0685d7f918ecbd607a4cdd1d2132b233f
Sotirios Mantziaris <s.mantziaris@live.com>
2016-03-23 21:57:41 +0200

    only tests where it makes sense, fixed merge coreages

--- mergecoverages.sh
+++ mergecoverages.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ev
 
+echo "mode: atomic" > substitute.coverprofile
+
 for pkg in `find . -name "*.coverprofile" -type f | grep -v 'substitute.coverprofile'`; do
     echo $pkg
     tail -n +2 $pkg >> substitute.coverprofile
